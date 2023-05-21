7471432aaa18dea144c000edc21dcfcc1ac47fa1
Isac Dev <50426537+isaukywhite@users.noreply.github.com>
2023-05-21 11:13:25 -0300

    update

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
-import 'package:ngdart/angular.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:ngdart/angular.dart';
 
 void main() {
   runApp(ng.AppComponentNgFactory);
